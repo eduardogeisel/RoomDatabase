b6c51932b5c4a34b163f340dc9a295298f009051
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-23 17:18:32 -0700

    Created entity class

---
+++ app/src/main/java/com/eduardogeiselperes/roomdatabase/data/Vehicle.kt
@@ -0,0 +1,13 @@
+package com.eduardogeiselperes.roomdatabase.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "vehicle_table")
+data class Vehicle(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val make: String,
+    val model: String,
+    val year: Int
+)
