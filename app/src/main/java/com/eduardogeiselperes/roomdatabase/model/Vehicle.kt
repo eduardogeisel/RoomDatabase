c4df30f68e3d68cc7df7f739c91145ba7ef706ec
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-23 23:04:04 -0700

    Working on navigation for update

--- app/src/main/java/com/eduardogeiselperes/roomdatabase/model/Vehicle.kt
+++ app/src/main/java/com/eduardogeiselperes/roomdatabase/model/Vehicle.kt
@@ -1,8 +1,11 @@
 package com.eduardogeiselperes.roomdatabase.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "vehicle_table")
 data class Vehicle(
     @PrimaryKey(autoGenerate = true)
@@ -10,4 +13,4 @@ data class Vehicle(
     val make: String,
     val model: String,
     val year: Int
-)
+): Parcelable
