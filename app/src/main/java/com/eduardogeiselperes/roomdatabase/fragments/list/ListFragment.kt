8db0b734be13e4d54f024633ec03d783ebfd4c50
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-23 21:22:07 -0700

    Changed labels for fragments

--- app/src/main/java/com/eduardogeiselperes/roomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/eduardogeiselperes/roomdatabase/fragments/list/ListFragment.kt
@@ -15,6 +15,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 }
