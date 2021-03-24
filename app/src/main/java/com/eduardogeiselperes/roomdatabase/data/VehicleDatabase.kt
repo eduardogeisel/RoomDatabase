f25b312848abd4d95228cfcb44715aefea4b99b8
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-23 17:57:15 -0700

    Created VehicleDatabase.kt

---
+++ app/src/main/java/com/eduardogeiselperes/roomdatabase/data/VehicleDatabase.kt
@@ -0,0 +1,33 @@
+package com.eduardogeiselperes.roomdatabase.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Vehicle::class], version = 1, exportSchema = false)
+abstract class VehicleDatabase: RoomDatabase() {
+
+    abstract fun vehicleDao(): VehicleDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: VehicleDatabase? = null
+
+        fun getDatabase(context: Context): VehicleDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    VehicleDatabase::class.java,
+                    "vehicle_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
